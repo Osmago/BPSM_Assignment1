720898b698245798aaf6a9b8ed42d6ca1403ced9
Felipe Buson <s2005863@bioinfmsc5.bio.ed.ac.uk>
2019-10-05 10:18:34 +0100

    script now gets/unzips the initial files paths and prints the files being used

--- Assignment1.sh
+++ Assignment1.sh
@@ -3,17 +3,26 @@
 #this script will get data from RNA sequencing of two life stages of Tripanosoma bruscei bruscei,
 #with three samples each, and count the mean count of reads aligning with each reference gene
 
-#the 'fqfiles' file will guide a loop in order to process the sequencing pairs one at a time
+#first set the variables relating to each initial file
 
+#fqfiles will guide a loop later to the paired reads files
 guide="/localdisk/data/BPSM/Assignment1/fastq/fqfiles"
-echo "$guide"
 
+#genome file must be decompressed to be used by bowtie2
+zip_genome="/localdisk/data/BPSM/Assignment1/Tbb_genome/Tb927_genome.fasta.gz"
+zcat $zip_genome > Tb927_genome.fasta
+genome="Tbb_genome.fasta"
+
+#genes file to generate counts in the end
+genes="/localdisk/data/BPSM/Assignment1/Tbbgenes.bed"
+
+#declare what are the files being used
+echo "using $guide file as guide for paired reads"
+echo "using $zip_genome file as genome"
+echo "using $genes file as reference for gene locations"
 
 while read smpl_nmbr smpl_type pair1 pair2; do
- if test -f /localdisk/data/BPSM/Assignment1/fastq/$pair1; then
-  echo "$pair1 exists"
-  fi
- if test -f /localdisk/data/BPSM/Assignment1/fastq/$pair2; then
- echo "$pair2 exists"
-  fi
+ pair1_path="/localdisk/data/BPSM/Assignment1/fastq/$pair1"
+ pair2_path="/localdisk/data/BPSM/Assignment1/fastq/$pair2"
+ 
 done < $guide
