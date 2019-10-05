c197ce53741b2392e110d35161f288eff7f8dd6a
Felipe Buson <s2005863@bioinfmsc5.bio.ed.ac.uk>
2019-10-05 11:45:55 +0100

    added fastqc processing and summary presentation for the user

--- Assignment1.sh
+++ Assignment1.sh
@@ -16,13 +16,26 @@ genome="Tbb_genome.fasta"
 #genes file to generate counts in the end
 genes="/localdisk/data/BPSM/Assignment1/Tbbgenes.bed"
 
-#declare what are the files being used
-echo "using $guide file as guide for paired reads"
-echo "using $zip_genome file as genome"
-echo "using $genes file as reference for gene locations"
+#make or confirm location for fastqc outputs, then clean it
+if test ! -d fastqc_outputs; then
+ mkdir fastqc_outputs
+ fi
+#rm -fr fastqc_outputs/*
 
-while read smpl_nmbr smpl_type pair1 pair2; do
+declare what are the files being used
+echo -e "\nUsing $guide\tfile as guide for paired reads"
+echo -e "Using $zip_genome\tfile as genome"
+echo -e "Using $genes\tfile as reference for gene locations\n"
+
+#sorts fqfiles just so we do everything in order, then pipes into data processing loop
+sort -k1,1n $guide | while read smpl_nmbr smpl_type pair1 pair2; do
  pair1_path="/localdisk/data/BPSM/Assignment1/fastq/$pair1"
  pair2_path="/localdisk/data/BPSM/Assignment1/fastq/$pair2"
- 
-done < $guide
+# echo -e "\nStarting read quality assessment of sample $smpl_nmbr\n"
+# fastqc --extract -o fastqc_outputs/ $pair1_path $pair2_path
+ echo -e "Results for quality assessmentof pair $smpl_nmbr:"
+ echo -e "\n$pair1"
+ cut -f1,2 fastqc_outputs/"$smpl_nmbr"_L8_1_fastqc/summary.txt
+ echo -e "\n$pair2"
+ cut -f1,2 fastqc_outputs/"$smpl_nmbr"_L8_2_fastqc/summary.txt
+done
